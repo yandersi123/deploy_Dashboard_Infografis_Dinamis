55b560ce5fdc907d1eacc732152e12517165bb58
Prayander Sahatma Siahaan <87557872+yandersi123@users.noreply.github.com>
2024-02-29 07:34:16 +0700

    Add files via upload

--- InfografisDinamis.py
+++ InfografisDinamis.py
@@ -3,7 +3,7 @@ import altair as alt
 import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
-import plotly.graph_objects as go
+#import plotly.graph_objects as go
 
 st.set_page_config(layout="wide")
 
